b44b295b7be2d6b8e7d59ed27e9b51be6ba8dd10
Bhaskar Subhash <bhaskarsubash9393@gmail.com>
2026-01-09 17:48:37 +0530

    Reconstruct Project Directories and Completed Sorter.sh to work

--- Scripts/sorter.sh
+++ Scripts/sorter.sh
@@ -10,7 +10,7 @@ for fileType in "${fileTypes[@]}"; do
   files=( $fileType )
 
   if [[ ${#files[@]} -ne 0 ]]; then
-    printf "Found %s %s files. List them? [y/N]: " "${#files[@]}" "$fileType"   ### TODO: small change: add color to the output and also print them in columns" ###
+    printf "Found %s %s files. List them? [y/N]: " "${#files[@]}" "${fileTypeNames[$fileType]}"   ### TODO: small change: add color to the output and also print them in columns" ###
     read choice
 
     if [[ $choice == [yY] ]]; then
@@ -20,15 +20,20 @@ for fileType in "${fileTypes[@]}"; do
     printf "Move the located files to their respective directory? [Y/n]: "
     read -r choice
     
-    if [[ choice == [yY] ]]; then
-      mv $fileType ${fileTypeDirs[$fileType]}
-    else
+    if [[ choice == [nN] ]]; then
       printf "Would you like to move these to a custom directory? [y/N]: "
       read -r choice
       if [[ choice == [yY] ]]; then
         read -r directory
+        mkdir -p $directory
         mv $fileType $directory
+      else
+        printf "Aborting operation for %s files.\n" "${fileTypeNames[$fileType]}"
+      fi 
+    else
+      mkdir -p ${fileTypeDirs[$fileType]}
+      echo "mv ${files[@]} ${fileTypeDirs[$fileType]}"
+      mv "${files[@]}" "${fileTypeDirs[$fileType]}"
+    fi
   fi
 done
-
-
--- Utils/filetypes.sh
+++ Utils/filetypes.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ### fileTypes dictionary contains all the file types for sorting
 
 fileTypes=(
